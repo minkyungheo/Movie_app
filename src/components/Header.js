fcde7889b001053bce4a27d11239d28f8c1c244f
허민경 <gjalsrud1234@gmail.com>
2025-06-11 18:37:53 +0900

    api 연동 및 헤더부분 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import {MarkGithubIcon} from '@primer/octicons-react'
-import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderBlock = styled.header`
+    position: sticky;
+    top: 0;
+    display:flex;
+    align-items: center;
     z-index :auto;
-    justify-content:space-between;
-    width: 1553px;
+    width: auto;
     height: 117px;
+    background-color:black;
 `;
 
 const Logo = styled.h1`
+    margin-left: 20px;
     font-size: 50px;
     color : #FF0A0E;
 
@@ -18,19 +22,33 @@ const Logo = styled.h1`
 const Nav = styled.nav`
     display: flex;
     gap:2px;
+    margin-left : 50px;
+    margin-top : 20px;
 `;
+const NavTab = styled.li`
+    list-style: none;
+    margin-right: 60px;
+    color: white;
+`;
+const GithubLink = styled.a`
+    margin-left: auto;
+    margin-right: 50px;
+`;
+
 export function Header(){
     return(
         <HeaderBlock>
             <Logo>NETFLIX</Logo>
             <Nav>
-                <li>홈</li>
-                <li>시리즈</li>
-                <li>영화</li>
+                <NavTab>홈</NavTab>
+                <NavTab>시리즈</NavTab>
+                <NavTab>영화</NavTab>
+                <NavTab>추천</NavTab>
             </Nav>
-            <a href="https://github.com/minkyungheo">
-                <MarkGithubIcon size={24} />
-            </a>
+            <GithubLink
+             href="https://github.com/minkyungheo">
+                <MarkGithubIcon size={24} color="white"/>
+            </GithubLink>
 
         </HeaderBlock>
 
